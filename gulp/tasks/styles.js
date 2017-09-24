11b59640a76aee3c2a4ab90a45ac34720135bdd6
Peter <peter.w570@gmail.com>
2017-09-24 21:12:55 +0100

    Mixins
    Added mixin dependency in gulp file. Mixin media queries for small, medium and large screens. Mobile-first CSS for large hero block title and subtitle

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,11 +3,12 @@ var gulp = require('gulp'),
 	autoprefixer = require('autoprefixer'),
 	cssVars = require('postcss-simple-vars'),
 	nestedCss = require('postcss-nested'),
-	cssImport = require('postcss-import');
+	cssImport = require('postcss-import'),
+	mixins = require('postcss-mixins');
 
 gulp.task('styles', function () {
 	return gulp.src('./app/assets/styles/styles.css')
-		.pipe(postcss([cssImport, cssVars, nestedCss, autoprefixer]))
+		.pipe(postcss([cssImport, mixins, cssVars, nestedCss, autoprefixer]))
 		.on('error', function(errorInfo) {
 			console.log(errorInfo.toString());
 			this.emit('end');
