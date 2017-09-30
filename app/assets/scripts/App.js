cdc53d1ed07eeee38ccde6d19a961355936ff17c
Peter <peter.w570@gmail.com>
2017-09-30 20:30:06 +0100

    JS for toggling a class when menu icon is clicked

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,16 +1,3 @@
-var $ = require('jquery');
-//var Person = require('./modules/Person');
-import Person from './modules/Person'
+import MobileMenu from './modules/MobileMenu';
 
-class Adult extends Person {
-	payTaxes() {
-		console.log(this.name + " now owes £0 in taxes.")
-	}
-}
-
-var peter = new Person("Peter Weightman", "Blue");
-var leia = new Adult("Leia Turner", "Purple");
-
-peter.greet();
-leia.greet();
-leia.payTaxes();
+var mobileMenu = new MobileMenu();
